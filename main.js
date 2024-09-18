cbe783615ac6cedf632512cb85570a09f6f922d3
Bahtiyor1 <bakhtierbek2011@gmail.com>
2024-09-18 18:09:38 +0500

    object id

---
+++ main.js
@@ -0,0 +1,25 @@
+// let Ism = prompt('Ismingizni yozing');
+// let Familiya = prompt('Familiyangizni yozing');
+// let obj = {
+//     ism: 'Behruz',
+//     familiya: 'Satimbayev'
+// };
+
+// if (obj.ism === Ism && obj.familiya === Familiya) {
+//     alert('Xush kelibsiz');
+// } else {
+//     alert('Daniy notogri kiritilgan');
+// }
+
+
+let name = prompt('ismingizni kiriting')
+let id = prompt('Space idingizni kiriting')
+let password = prompt('Space passwordingzini kiriting')
+let obj = {
+    name: `${name}`,
+    id: `${id}`,
+    password: `${password}`
+}
+
+console.log(obj)
+
